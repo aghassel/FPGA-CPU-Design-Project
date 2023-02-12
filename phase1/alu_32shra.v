27bde3c9ac829785bb748fbfde1d84df58b1de22
Abdellah Ghassel <19aag9@queensu.ca>
2023-02-11 19:09:53 -0500

    optimized shla, shra, and made comments

--- phase1/alu_32shra.v
+++ phase1/alu_32shra.v
@@ -1,23 +1,33 @@
-module ror32 (in, num_shifts, out);
+module shra32 (in, num_shifts, out);
 	input wire [31:0] in;
 	input wire [31:0] num_shifts;
 	output reg [31:0] out;
 	
-    wire temp;
-    genvar i;
-    integer shifts;
 	always @(*) begin
-        shifts = num_shifts;
-        generate
-            for (i=0; i < shifts, i = i + 1) begin : loop
-                if (in[31] = 0)
-                    assign in = in>>1;
-                else if (in[31] = 1) begin
-                    assign in = in>>1;
-                    assign in[31] = 1;
-                end
-            end
-            assign out = in;
-        endgenerate
+		assign out[31:0] = in>>>num_shifts;
 	end
 endmodule
+
+// module ror32 (in, num_shifts, out);
+// 	input wire [31:0] in;
+// 	input wire [31:0] num_shifts;
+// 	output reg [31:0] out;
+	
+//     wire temp;
+//     genvar i;
+//     integer shifts;
+// 	always @(*) begin
+//         shifts = num_shifts;
+//         generate
+//             for (i=0; i < shifts, i = i + 1) begin : loop
+//                 if (in[31] = 0)
+//                     assign in = in>>1;
+//                 else if (in[31] = 1) begin
+//                     assign in = in>>1;
+//                     assign in[31] = 1;
+//                 end
+//             end
+//             assign out = in;
+//         endgenerate
+// 	end
+// endmodule
