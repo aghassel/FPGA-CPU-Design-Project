1bfb2005e80239d114e4e7d7f5c2952b0f919b10
Abdellah Ghassel <19aag9@queensu.ca>
2023-02-12 16:08:35 -0500

    fixed syntax errors in alu

--- phase1/alu.v
+++ phase1/alu.v
@@ -1,4 +1,3 @@
-
 module alu (A, B, opcode, clk, clr, control, C);
     input wire [31:0] A, B, control;
     input wire clk, clr;
@@ -9,7 +8,6 @@ module alu (A, B, opcode, clk, clr, control, C);
     reg [63:0] mul_reg, div_reg;
     
             
-
     parameter add = 5'b00001, sub = 5'b00010, mul = 5'b00011, div = 5'b00100, shr = 5'b00101, shl = 5'b00110, shra = 5'b00111, 
                     ror = 5'b01000, rol = 5'b01001, log_and = 5'b01010, log_or = 5'b01011, log_neg = 5'b01100, log_xor = 5'b01101,
                     log_nor = 5'b01110, log_not = 5'b01111; 
@@ -20,7 +18,7 @@ module alu (A, B, opcode, clk, clr, control, C);
     //alu_32div divop (.a(A), .b(B), .s(mul_reg));
     alu_32and andop (.a(A), .b(B), .c(and_reg));
     alu_32or orop (.a(A), .b(B), .c(or_reg));
-    alu_32xor xorop (.a(A), b(B), .c(xor_reg));
+    alu_32xor xorop (.a(A), .b(B), .c(xor_reg));
     alu_32neg negop (.in(B), .out(neg_reg));
     alu_32nor norop (.in(B), .out(nor_reg));
     alu_32not notop (.in(B), .out(not_reg));
@@ -31,10 +29,8 @@ module alu (A, B, opcode, clk, clr, control, C);
     alu_32rol rolop (B, A, rol_reg);
     
 
-
-
     always @(posedge clk) begin
-        if (opcode) begin
+        //if (opcode) begin -- don't need this since ur using cases
             case(opcode) 
                 add: begin
                     C [31:0] <= add_reg[31:0];
@@ -94,8 +90,11 @@ module alu (A, B, opcode, clk, clr, control, C);
                     C [31:0] <= not_reg[31:0];
                     C [63:32] <= 32'd0;
                 end
+                default: begin
+                    C <= 64'd0;
+                end
             endcase 
         end
-    end
+    //end
 endmodule
 
