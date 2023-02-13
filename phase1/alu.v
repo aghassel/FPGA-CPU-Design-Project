b0713e53e269672e878c7f7df51e5840718dc734
Liam <liam.salass@queensu.ca>
2023-02-13 16:46:50 -0500

    partial booths implementation

--- phase1/alu.v
+++ phase1/alu.v
@@ -2,7 +2,7 @@ module alu (A, B, opcode, clk, clr, control, C);
     input wire [31:0] A, B, control;
     input wire clk, clr;
     input wire [4:0] opcode;
-    output [63:0] C;
+    output reg [63:0] C;
     
     reg[31:0] neg_reg, nor_reg, or_reg, not_reg, xor_reg, and_reg, shl_reg, shr_reg, shra_reg, ror_reg, rol_reg, add_reg, sub_reg,
     add_cout_reg, sub_cout_reg;
--- phase1/alu_32mul.v
+++ phase1/alu_32mul.v
@@ -1,28 +1,47 @@
 //Booths algorithm --unfinished
 
-module multi32 (a,b,,cout,s);
-	input signed [31:0] a, b;
-	output signed [63:0] z);
-	output wire c;
-	reg signed [31:0] z, z_next, z_temp;
-	reg next_state, present_state;
-	reg [1:0] temp, next_temp;
-	reg[1:0] count, next_count; 
-	reg valid, next_valid;
-	
-	parameter IDLE = 1'b0;
-	parameter START = 1b'1;
+module multi32 (a,b,p);
+	input [31:0] a,b;
+	output [63:0] p;
+	reg [32:0] mul
 
-	always @(*) begin
-		z<= 64'd0;
-		valid <= 1'b0;
-		present_state <= 1'b0;
-		temp <= 2'd0;
-		count <=	2'd0;
 
-	endmodule
-		
-		
-		 
+	always @(*)begin
+		mul[32:1] <= a[31:0];
+		mul[0] <= 1'b0;
+		p[63:0] <= 64'd0; 
+		genvar i;
+		reg [63:0] pp;
+		integer s;
+		s = 0;
+		generate
+			for (i=0; i < 11, i = i+1) begin : loop
+				pp[63:0] = 64'd0
+				case (mul[2:0])
+					3'b001, 3'b010 : begin // +1 X M
+						pp[31:0] = b[31:0];	
+						if (pp[31] = 1'b1) pp[63:32] = 32'hFFFFFFFF;
+					end 						
+					3'b011 : begin // +2 X M
+						pp[32:0] = (b[31:0] << 1);	
+						if (pp[32] = 1'b1) pp[63:32] = 32'hFFFFFFFF;
+					end									
+					3'b100 : begin // -2 X M
+						pp[32:0] = ((-b[31:0]+1)<<1);	
+						if (pp[32] = 1'b1) pp[63:32] = 32'hFFFFFFFF;
+					end									
+					3'b101, 3'b110 : pp[63:0] = ((-b[31:0]+1)) 						// -1 X M
+					default : pp[63:0] = 64'd0							//  0 X M
+				endcase
+				s = s + 2;
+				mul[32:0] = (a[32:0]>>3);
+			end
+
+		endgenerate
+
+
+	end
+endmodule
+
 		
 		
