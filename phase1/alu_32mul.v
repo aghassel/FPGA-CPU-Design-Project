2ba8d3d6574391c07e511a33ca45de6b0a7c8260
Liam <liam.salass@queensu.ca>
2023-02-13 17:30:11 -0500

    Almost finished

--- phase1/alu_32mul.v
+++ phase1/alu_32mul.v
@@ -1,45 +1,53 @@
 //Booths algorithm --unfinished
 
 module multi32 (a,b,p);
-	input [31:0] a,b;
-	output [63:0] p;
-	reg [32:0] mul
-
+	input signed reg [31:0] a,b;
+	output signed reg[63:0] p;
+	reg [2:0] mul
+	reg [31:0] neg_b;
+	integer i;
+	reg [63:0] pp;
+	integer s;
 
 	always @(*)begin
-		mul[32:1] <= a[31:0];
-		mul[0] <= 1'b0;
-		p[63:0] <= 64'd0; 
-		genvar i;
-		reg [63:0] pp;
-		integer s;
+		mul[2:1] = a[2:1];
+		mul[0] = 1'b0;
+		p[63:0] = 64'd0; 
+		
 		s = 0;
-		generate
-			for (i=0; i < 11, i = i+1) begin : loop
-				pp[63:0] = 64'd0
-				case (mul[2:0])
-					3'b001, 3'b010 : begin // +1 X M
-						pp[31:0] = b[31:0];	
-						if (pp[31] = 1'b1) pp[63:32] = 32'hFFFFFFFF;
-					end 						
-					3'b011 : begin // +2 X M
-						pp[32:0] = (b[31:0] << 1);	
-						if (pp[32] = 1'b1) pp[63:32] = 32'hFFFFFFFF;
-					end									
-					3'b100 : begin // -2 X M
-						pp[32:0] = ((-b[31:0]+1)<<1);	
-						if (pp[32] = 1'b1) pp[63:32] = 32'hFFFFFFFF;
-					end									
-					3'b101, 3'b110 : pp[63:0] = ((-b[31:0]+1)) 						// -1 X M
-					default : pp[63:0] = 64'd0							//  0 X M
-				endcase
-				s = s + 2;
-				mul[32:0] = (a[32:0]>>3);
-			end
+		neg_b = ((~b[31:0])+1'b1);
 
-		endgenerate
+		for (i=0; i < 16, i = i+1) begin 
+			pp[63:0] = 64'd0
+			case (mul[2:0])
+				3'b001, 3'b010 : begin // +1 X M
+					pp [31:0] = b [31:0];
+					if (pp[31] = 1'b1) pp[63:32] = 32'hFFFFFFFF
+				end 						
+				3'b011 : begin // +2 X M
+					pp [31:0] = b [31:0];
+					if (pp[31] = 1'b1) pp[63:32] = 32'hFFFFFFFF
+					pp = pp << 1;
 
+				end									
+				3'b100 : begin // -2 X M
+					pp [31:0] = neg_b[31:0];
+					if (pp[31] = 1'b1) pp[63:32] = 32'hFFFFFFFF
+					pp = pp << 1;
+				end									
+				3'b101, 3'b110 : begin // -1 X M
+					pp [31:0] = neg_b[31:0];
+					if (pp[31] = 1'b1) pp[63:32] = 32'hFFFFFFFF
+				end			
+				default : pp[63:0] = 64'd0							//  0 X M
+			endcase
 
+			pp = pp << s;
+			s = s + 2;
+			a = a >> 2
+			mul[2:0] = a[2:0]
+			p = p + pp;
+		end
 	end
 endmodule
 
