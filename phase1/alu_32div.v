bc6462f5e6824cfa2461cb5f7cc20c58187eade7
Abdellah Ghassel <19aag9@queensu.ca>
2023-02-15 13:25:30 -0500

    syntax errors resolved

--- phase1/alu_32div.v
+++ phase1/alu_32div.v
@@ -1,13 +1,11 @@
 module div #(parameter wordSize = 32)(
-	input [wordsize-1:0]dividend, divisor,
+	input [wordSize-1:0]dividend, divisor,
 	output [wordSize*2-1:0] out
 	);
 	//Z register is 64-bit: remainder in the higher byte, and quotient in the lower byte) operation
-	input wire h, l;
+	wire [wordSize-1:0] h, l;
 
-	always @(*) begin
-		h = dividend % divisor		//remainder
-		l = (dividend - h) / divisor //quotient
-		out = {h,l} //concatenate high and low in output
-	end
+		h = dividend % divisor;		//remainder
+		l = (dividend - h) / divisor; //quotient
+		out = {h,l}; //concatenate high and low in output
 endmodule
--- phase1/alu_32shra.v
+++ phase1/alu_32shra.v
@@ -1,40 +1,16 @@
-module shra32 (in, num_shifts, out);
-	input wire [31:0] in;
-	input wire [31:0] num_shifts;
-	output reg [31:0] out;
-	
-	always @(*) begin
-
-        shifts = num_shifts;
-        generate
-            for (i=0; i < shifts, i = i + 1) begin : loop
-                if (in[31] = 0)
-                    assign in = in>>1;
-                else begin
-                    assign in = in>>1;
-                    assign in[31] = 1;
-                end
-            end
-            assign out = in;
-        endgenerate
-	end
-endmodule
-
-// module ror32 (in, num_shifts, out);
+// module shra32 (in, num_shifts, out);
 // 	input wire [31:0] in;
 // 	input wire [31:0] num_shifts;
 // 	output reg [31:0] out;
 	
-//     wire temp;
-//     genvar i;
-//     integer shifts;
 // 	always @(*) begin
+
 //         shifts = num_shifts;
 //         generate
 //             for (i=0; i < shifts, i = i + 1) begin : loop
 //                 if (in[31] = 0)
 //                     assign in = in>>1;
-//                 else if (in[31] = 1) begin
+//                 else begin
 //                     assign in = in>>1;
 //                     assign in[31] = 1;
 //                 end
@@ -43,3 +19,13 @@ endmodule
 //         endgenerate
 // 	end
 // endmodule
+
+module shra32 (in, num_shifts, out);
+	input wire signed [31:0] in;
+	input wire [31:0] num_shifts;
+	output reg signed [31:0] out;
+	
+	always @(*) begin
+		out[31:0] = in>>>num_shifts;
+	end
+endmodule
