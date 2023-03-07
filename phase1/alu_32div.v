2493cd0dbe7345a1a4771d4a1bd07f1e36c72530
LiamSalass <liamsalass@yahoo.com>
2023-03-07 17:55:02 -0500

    added new division algorithm

--- phase1/alu_32div.v
+++ phase1/alu_32div.v
@@ -1,11 +1,29 @@
-module div #(parameter wordSize = 32)(
-	input signed[wordSize-1:0]dividend, divisor,
-	output signed[wordSize*2-1:0] out
-	);
-	//Z register is 64-bit: remainder in the higher byte, and quotient in the lower byte) operation
-	wire [wordSize-1:0] h, l;
-
-	assign h = dividend % divisor;		//remainder
-	assign l = (dividend - h) / divisor; //quotient
-	assign out = {h,l}; //concatenate high and low in output
+module divider(
+    input [31:0] dividend_input,
+    input [31:0] divisor_input,
+    output reg [31:0] quotient_output,
+    output reg [31:0] remainder_output
+);
+always @* begin
+    integer i;
+    
+    // initialize remainder with dividend
+    remainder_output = dividend_input;
+    
+    // initialize quotient with 0
+    quotient_output = 0;
+    
+    // iterate from MSB to LSB
+    for (i = 31; i >= 0; i=i-1) begin
+        
+        // check if remainder is greater than or equal to divisor shifted left by i bits
+        if (remainder_output >= (divisor_input << i)) begin
+		  // subtract shifted divisor from remainder
+            remainder_output = remainder_output - (divisor_input << i);
+            
+            // set the ith bit of quotient to 1
+            quotient_output[i] = 1'b1;
+        end
+    end
+end
 endmodule
