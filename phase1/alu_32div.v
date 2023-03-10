7faec47f20f3b16533e7804e092a704bc722ab77
Liam <liam.salass@queensu.ca>
2023-03-09 20:56:29 -0500

    Pushed div

--- phase1/alu_32div.v
+++ phase1/alu_32div.v
@@ -1,10 +1,10 @@
 module div32(
     input [31:0] D,
-    input [31:0] O,
+    input [31:0] V,
     output reg [31:0] Q,
     output reg [31:0] R
 );
-always @* begin
+always @(*) begin
     integer i;
     
     // initialize remainder with dividend
@@ -16,9 +16,9 @@ always @* begin
     for (i = 31; i >= 0; i=i-1) begin
         
         // check if remainder is greater than or equal to divisor shifted left by i bits
-        if (R >= (O << i)) begin
+        if (R >= (V << i)) begin
 		  // subtract shifted divisor from remainder
-            R = R - (O << i);
+            R = R - (V << i);
             
             // set the ith bit of quotient to 1
             Q[i] = 1'b1;
