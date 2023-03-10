e59f9bd2df60be39a86a054d383728fec0ba95ed
Abdellah Ghassel <19aag9@queensu.ca>
2023-03-10 11:47:09 -0500

    datapath

--- abdellah_phase1/datapath.v
+++ abdellah_phase1/datapath.v
@@ -2,9 +2,12 @@ module datapath (
 input clr, clk,
 input R0in, R1in, R2in, R3in, R4in, R5in, R6in, R7in, R8in, R9in, R10in, R11in, R12in, R13in, R14in, R15in, 
 input HIin, LOin, Zin, incPC, MARin, MDRin, Read, InPortin, Cin, Yin,
+
+input R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out,
+input HIout, LOout, ZLowOut, ZHighOut, MARout, MDRout, Cout, Yout, InPortOut,
+
 input [4:0] opcode,
-input [31:0] Mdatain,
-input
+input [31:0] Mdatain
 );
 
 wire [31:0] BusMuxIn_R0, 
