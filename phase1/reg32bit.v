1edace55ba2de848be1d96a6b40188234de0f776
Liam <liam.salass@queensu.ca>
2023-03-10 22:59:29 -0500

    Added comments

--- phase1/reg32bit.v
+++ phase1/reg32bit.v
@@ -2,12 +2,11 @@ module reg32bit(clr, clk, enable, D, Q);
 	input wire clr, clk, enable;
 	input wire [31:0]D;
 	output reg [31:0]Q;
-
-	
+	//At positive clock edge begin
 	always @(posedge clk) begin
-		if (clr)
+		if (clr) //If clr is high set to 0
 			Q <= 0;
-		else if (enable)
+		else if (enable) //If enable is high, read value from bus to Q
 			Q <= D;
 	end
 endmodule
