38a68f0370227b1c5758f15e17c3a54715492a5c
Liam <liam.salass@queensu.ca>
2023-03-13 18:07:15 -0400

    Added new R0 architecture

---
+++ REGISTERS/regR0.v
@@ -0,0 +1,19 @@
+module reg32bit(clr, clk, enable, D, Q, BAout);
+	input wire clr, clk, enable;
+	input wire [31:0]D;
+	output reg [31:0]Q;
+	//At positive clock edge begin
+	always @(posedge clk) begin
+		if (clr) //If clr is high set to 0
+			Q <= 0;
+		else if (enable) //If enable is high, read value from bus to Q
+            Q <= D;
+	end
+    always @(*) begin
+        if (~BAout)
+            Q <= 0;
+    end
+
+endmodule
+	
+	
