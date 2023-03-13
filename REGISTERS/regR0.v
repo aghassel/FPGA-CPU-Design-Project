f1ff17d26a980c8704d7d00956bd241a8e70226f
Liam <liam.salass@queensu.ca>
2023-03-13 19:55:19 -0400

    Finished regR0

--- REGISTERS/regR0.v
+++ REGISTERS/regR0.v
@@ -3,15 +3,19 @@ module reg32bit(BAout, clr, clk, enable, D, Q);
 	input wire [31:0]D;
 	output reg [31:0]Q;
 	//At positive clock edge begin
+	wire regout;
+	
 	always @(posedge clk) begin
 		if (clr) //If clr is high set to 0
-			Q <= 0;
+			regout <= 0;
 		else if (enable) //If enable is high, read value from bus to Q
-            Q <= D;
+            regout <= D;
 	end
     always @(*) begin
         if (~BAout)
             Q <= 0;
+		else
+			Q <= regout;
     end
 
 endmodule
