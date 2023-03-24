1530c810f66e3ae9beda80742936278a6dbfccc8
aghassel <aghassel@gmail.com>
2023-03-23 23:25:37 -0400

    PC changes

--- REGISTERS/regPC.v
+++ REGISTERS/regPC.v
@@ -6,13 +6,14 @@ module regPC #(parameter qInitial = 0)(clr, clk, incPC, enable, D, Q);
 	reg [31:0] temp;
 	
 	initial Q = qInitial;
-	
 
     //Use posedge clk and check for incPC inside the always block
 	always @(posedge clk) begin
-		if (enable && incPC) //If clr is high set to 0
+		temp = Q;
+		
+		if (clr) //If clr is high set to 0
 			Q <= 0;
 		else if (enable) //If enable is high, read value from bus to Q
-			Q <= temp;
+			Q = (incPC) ? temp + 1 : temp;
 	end
 endmodule
