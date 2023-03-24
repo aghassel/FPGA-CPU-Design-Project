2fecf47b0a7b1120e8e5b34388e270f6be8ec7b8
Liam <liam.salass@queensu.ca>
2023-03-23 23:37:33 -0400

    fixed pcreg

--- REGISTERS/regPC.v
+++ REGISTERS/regPC.v
@@ -3,16 +3,21 @@ module regPC #(parameter qInitial = 0)(clr, clk, incPC, enable, D, Q);
 	input wire [31:0]D;
 	output reg [31:0]Q;
 	
-	reg [31:0] temp;
+	reg [31:0] next_Q;
 	
 	initial Q = qInitial;
 	
-
+	always @(posedge incPC) begin
+		next_Q = Q + 1;
+	end
+	
     //Use posedge clk and check for incPC inside the always block
 	always @(posedge clk) begin
-		if (enable && incPC) //If clr is high set to 0
+		if (clr) //If clr is high set to 0
 			Q <= 0;
 		else if (enable) //If enable is high, read value from bus to Q
-			Q <= temp;
+			Q <= D;
+		else
+			Q <= next_Q;
 	end
 endmodule
