3a16065f10dd233460c809bfc8e05ca06a3e49b1
Liamsalass <liamsalass@yahoo.com>
2023-03-29 16:46:31 -0400

    finished tb

--- TESTBENCH/phase3_tb/tb_minisrc.v
+++ TESTBENCH/phase3_tb/tb_minisrc.v
@@ -3,8 +3,12 @@
 `timescale 1ns/1ps
 
 module tb_minisrc;
-    reg run, clear, clock, reset, stop;
+    //Inputs
+    reg run, clear, clock; 
     reg [31:0] InPortData;
+    //Outputs
+    reg reset, stop; 
+
 
     datapath dp (
         .run(run),
@@ -15,7 +19,13 @@ module tb_minisrc;
         .InPortData(InPortData)
         );
 
-    
+    initial begin
+		clk = 0; reset = 1;
+        #2 clk = 1;
+        #2 clk = 0; reset = 0;
+        #2 clk = 1;
+		forever #2 clk = ~clk;
+	end
 
 
 endmodule
